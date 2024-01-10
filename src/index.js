293a420f227db70983390e717dc3a164df164ce7
Abdul Quim <abdul.quim99@gmail.com>
2024-01-10 15:19:54 +0500

    Feat: Added Rate Limiter and Reverse Proxy in API Gateway

--- src/index.js
+++ src/index.js
@@ -1,11 +1,40 @@
 const express = require("express");
+const ratelimit = require("express-rate-limit");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const { ServerConfig, LoggerConfig } = require("./config");
 const Routes = require("./routes");
 
 const app = express();
+const limiter = ratelimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(limiter);
+
+app.use(
+  "/flightService",
+  createProxyMiddleware({
+    target: ServerConfig.FLIGHTS_SERVICE,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/flightService": "/", // rewrite path
+    },
+  })
+);
+
+app.use(
+  "/bookingService",
+  createProxyMiddleware({
+    target: ServerConfig.BOOKINGS_SERVICE,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/bookingService": "/", // rewrite path
+    },
+  })
+);
 
 app.use("/api", Routes);
 
